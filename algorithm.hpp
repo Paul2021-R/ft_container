47bb8bcd5aef10e68d32b27b843f324501fb774e
fallen8 <sorksoek2@gmail.com>
2022-12-02 18:43:50 +0900

    implement lexicographical_compare

--- algorithm.hpp
+++ algorithm.hpp
@@ -6,7 +6,7 @@
 /*   By: seojin <seojin@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 13:36:22 by seojin            #+#    #+#             */
-/*   Updated: 2022/12/02 13:45:24 by seojin           ###   ########.fr       */
+/*   Updated: 2022/12/02 18:43:16 by seojin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@
 namespace ft
 {
 
+
+/* ====== equal ====== */
 template <class InputIt1, class InputIt2>
 bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2)
 {
@@ -43,6 +45,37 @@ bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p 
 	}
 	return true;
 }
+
+
+/* ====== lexicographical_compare ====== */
+template <class InputIt1, class InputIt2>
+bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2)
+{
+	while (first1 != last1)
+	{
+		if (first2==last2 || *first1 > *first2) return false;
+		else if (*first1 < *first2) return true;
+		++first1;
+		++first2;
+	}
+	return (first2 != last2);
+}
+
+
+template <class InputIt1, class InputIt2, class Compare>
+bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Compare compare)
+{
+	while (first1 != last1)
+	{
+		if (first2==last2 || compare(*first2, *first1)) return false;
+		else if (compare(*first1, *first2)) return true;
+		++first1;
+		++first2;
+	}
+	return (first2 != last2);
+}
+
+
 
 
 
